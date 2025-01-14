fb84d22acadd23153e93e9de6d547afbbbd7a3e0
LordCartel007 <cartellord77@gmail.com>
2025-01-14 10:58:57 +0100

    changed env name

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,7 +12,7 @@ export const authOptions = {
     // OAuth authentication providers...
 
     GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
